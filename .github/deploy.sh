1c3d0d4a9141b6c494969c4a1bfbfc090bfcb6bb
Jimmy Wärting <jimmy@warting.se>
2021-10-10 11:30:54 +0200

    set config

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -5,17 +5,29 @@ then
    exit 0
 fi
 
+username=$(git --no-pager log --format=format:'%an' -n 1)
+email=$(git --no-pager log --format=format:'%ae' -n 1)
+repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+echo "Username: $username"
+echo "email: $username"
+echo "GITHUB_TOKEN: $GITHUB_TOKEN"
+
 GIT_REPO_URL=$(git config --get remote.origin.url)
 
 mkdir .deploy
 cp -R .github/actions/converter-docker-action/public/* .deploy
 cd .deploy
 git init .
-git remote add github $GIT_REPO_URL
+echo "setup credentials"
+git config --global user.name "$username"
+git config --global user.email "$email"
+git remote add pages $repo_uri
+echo "pushing"
 git checkout -b gh-pages
 git add .
 git commit -am "Create gh-pages"
-git push github gh-pages
+git push pages gh-pages
 cd ..
 rm -rf .deploy
 

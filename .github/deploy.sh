69132fe66427af0424a85bbf52a74e3cea931069
warting <stefan@warting.se>
2021-10-10 15:21:23 +0200

    test

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -5,6 +5,22 @@ then
    exit 0
 fi
 
+
+# fail on unset variables and command errors
+set -eu -o pipefail # -x: is for debugging
+
+DEFAULT_BRANCH="main"
+
+CURRENT_BRANCH="$(git branch --show-current)"
+if [ "${CURRENT_BRANCH}" != "${DEFAULT_BRANCH}" ]; then
+  echo "$0: Current branch ${CURRENT_BRANCH} is not ${DEFAULT_BRANCH}, continue? (y/n)"
+  read -r res
+  if [ "${res}" = "n" ]; then
+    echo "$0: Stop script"
+    exit 0
+  fi
+fi
+
 username=$(git --no-pager log --format=format:'%an' -n 1)
 email=$(git --no-pager log --format=format:'%ae' -n 1)
 repo_uri="https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/${GITHUB_REPOSITORY}.git"
@@ -20,8 +36,8 @@ cp -R .github/actions/converter-docker-action/public/* .deploy
 cd .deploy
 git init .
 echo "setup credentials"
-git config --global user.name "$username"
-git config --global user.email "$email"
+git config user.name "$username"
+git config user.email "$email"
 git remote add pages $repo_uri
 echo "pushing"
 
